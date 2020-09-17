ee8cc07c891d58b0176c3bcbb5b39eb338f1a578
deanbay <deanbaythoon@gmail.com>
2020-09-17 23:07:47 +0100

    Add files via upload

---
+++ Sqr.java
@@ -0,0 +1,31 @@
+
+public class Sqr {
+
+	private int length;
+	private int width;
+	
+	//The getters
+	
+	public int getLength() {
+		return length;
+	}
+	
+	public int getWidth() {
+		return width;	}
+	
+	public double getArea() {
+		int getArea = (length*width);
+		return getArea;
+	}
+	
+	//The setters
+	
+	public void setLength(int L) {
+		length = L;
+	}
+	
+	public void setWidth(int W) {
+		width = W;
+	}
+	
+}
